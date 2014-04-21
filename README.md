9ef3f0bc7b4be448ec5eeef268c4b5a2010f672b
Cong <congusbongus@gmail.com>
2014-04-21 23:13:41 +1000

    Code dump, update readme

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-RonnyWesterPics
+Ronny Wester Pics
 ===============
 
-Decoder for graphics files in Ronny Wester games
+Ronny Wester wrote a few DOS games in the 90s - C-Dogs, Cyberdogs, Magus. These games used a simple paletted image format in a single binary file.
+
+This utility decodes these graphics files (.px extension in C-Dogs and Cyberdogs) and outputs individual bitmap files. Note that some colours may not work because the colours themselves are palette indices that will be swapped in game.
+
+Utility uses [libbmp](https://code.google.com/p/libbmp/) (LGPL) to produce bitmap files.
+
+## To use
+Simply drag a graphics file over the executable `ronnypix`, or run:
+
+    ronnypix graphicsfile.px
+
+Bitmaps will be created in the current directory.
+
+Known games and files that work:
+
+**C-Dogs**
+
+-  cdogs.px
+-  cdogs2.px
+-  font.px
+
+**Cyberdogs**
+
+- dogs.px
---
+++ main.c
@@ -0,0 +1,52 @@
+#include "bmpfile.h"
+#include "pic_file.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define CLAMP(v, _min, _max) MAX((_min), MIN((_max), (v)))
+
+void ReadPicsFile(const char *filename)
+{
+	Pic **pics = calloc(9999, sizeof(Pic *));
+	TPalette palette;
+	int i;
+	int picsRead = ReadPics(filename, pics, palette);
+	for (i = 0; i < picsRead; i++)
+	{
+		Pic *pic = pics[i];
+		if (pic != NULL)
+		{
+			bmpfile_t *bmp = bmp_create(pic->w, pic->h, 8);
+			int j;
+			char outName[256];
+			for (j = 0; j < pic->w * pic->h; j++)
+			{
+				int x = j % pic->w;
+				int y = j / pic->w;
+				rgb_pixel_t pixel;
+				int paletteIndex = *((char *)&pic->data + j);
+				int gamma = 4;
+				pixel.blue = (uint8_t)CLAMP(palette[paletteIndex].blue * gamma, 0, 255);
+				pixel.green = (uint8_t)CLAMP(palette[paletteIndex].green * gamma, 0, 255);
+				pixel.red = (uint8_t)CLAMP(palette[paletteIndex].red * gamma, 0, 255);
+				pixel.alpha = 255;
+				bmp_set_pixel(bmp, x, y, pixel);
+			}
+			sprintf(outName, "%s%d.bmp", filename, i);
+			bmp_save(bmp, outName);
+			bmp_destroy(bmp);
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	for (int i = 1; i < argc; i++)
+	{
+		ReadPicsFile(argv[i]);
+	}
+	return 0;
+}
