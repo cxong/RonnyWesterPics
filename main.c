7380be1a2e97a7cb02be5dea1c0079b07b7c47aa
Cong <congusbongus@gmail.com>
2015-11-21 12:48:02 +1100

    Decode magus world.mgs

--- main.c
+++ main.c
@@ -15,7 +15,9 @@
 
 TPalette palette;
 
-static void PicPalettedToBMP(const PicPaletted *pic, const TPalette palette);
+static void PicPalettedToBMP(
+	const PicPaletted *pic, const TPalette palette,
+	const char *filename, const int i, const int gamma);
 
 int ReadPicsFile(const char *filename, const bool hasPalette)
 {
@@ -34,21 +36,29 @@ int ReadPicsFile(const char *filename, const bool hasPalette)
 	return picsRead;
 }
 
-void ReadMagusArt(const char *filename);
+void ReadMagusArt(
+	const char *filename, color_t palette[16], PicPaletted *pics[200]);
+void ReadMagusWorld(
+	const char *filename,
+	const color_t palette[16], const PicPaletted *pics[200]);
 
 int main(int argc, char *argv[])
 {
+	color_t magusPalette[16];
+	PicPaletted *magusPics[200];
+	memset(magusPics, 0, sizeof magusPics);
 	for (int i = 1; i < argc; i++)
 	{
 		printf("Reading %s...", argv[i]);
 		if (strcasecmp(argv[i] + strlen(argv[i]) - 4, ".art") == 0)
 		{
 			// magus.art
-			ReadMagusArt(argv[i]);
+			ReadMagusArt(argv[i], magusPalette, magusPics);
 		}
 		else if (strcasecmp(argv[i] + strlen(argv[i]) - 4, ".mgs") == 0)
 		{
 			// world.mgs
+			ReadMagusWorld(argv[i], magusPalette, magusPics);
 		}
 		else
 		{
@@ -57,10 +67,15 @@ int main(int argc, char *argv[])
 			printf("read %d\n", read);
 		}
 	}
+	for (int i = 0; i < 200; i++)
+	{
+		free(magusPics[i]);
+	}
 	return 0;
 }
 
-void ReadMagusArt(const char *filename)
+void ReadMagusArt(
+	const char *filename, color_t palette[16], PicPaletted *pics[200])
 {
 	FILE *f = fopen(filename, "rb");
 	if (f == NULL)
@@ -77,7 +92,6 @@ void ReadMagusArt(const char *filename)
 	}
 	FREAD(pal, sizeof pal);
 	// Convert to 0-255 range
-	color_t palette[16];
 	for (int i = 0; i < 16; i++)
 	{
 		// RGB stored in two bytes:
@@ -114,17 +128,17 @@ void ReadMagusArt(const char *filename)
 		uint16_t h;
 		FREAD(&h, sizeof h);
 		h++;
-		PicPaletted *pic = malloc(2 + 2 + w * h);
-		pic->w = w;
-		pic->h = h;
+		pics[i] = malloc(2 + 2 + w * h);
+		pics[i]->w = w;
+		pics[i]->h = h;
 
 		// Pixels stored in planar mode, so convert from that to regular pixels
 		char *buf = malloc(size - 4);
 		FREAD(buf, size - 4);
-		int planeSize = (pic->w + 7) / 8;
-		for (int x = 0; x < pic->w; x++)
+		int planeSize = (pics[i]->w + 7) / 8;
+		for (int x = 0; x < pics[i]->w; x++)
 		{
-			for (int y = 0; y < pic->h; y++)
+			for (int y = 0; y < pics[i]->h; y++)
 			{
 				int result = 0;
 				int rowStartsAt = (y * planeSize * 8) / 2;
@@ -137,13 +151,12 @@ void ReadMagusArt(const char *filename)
 					int index = rowStartsAt + (pnum * planeSize) + byteId;
 					result |= (buf[index] >> shift) & 0x01;
 				}
-				pic->data[x + y*pic->w] = result;
+				pics[i]->data[x + y*pics[i]->w] = result;
 			}
 		}
 		free(buf);
 
-		PicPalettedToBMP(pic, palette, filename, i, 1);
-		free(pic);
+		PicPalettedToBMP(pics[i], palette, filename, i, 1);
 	}
 
 bail:
@@ -152,6 +165,64 @@ bail:
 	{
 		fclose(f);
 	}
+}
+
+void ReadMagusWorld(
+	const char *filename,
+	const color_t palette[16], const PicPaletted *pics[200])
+{
+	PicPaletted *pic = NULL;
+	FILE *f = fopen(filename, "rb");
+	if (f == NULL)
+	{
+		printf("Error: cannot open file %s\n", filename);
+		goto bail;
+	}
+
+	const int tw = 24;
+	const int th = 21;
+	const int w = 200;
+	const int h = 320;
+	const int mw = w * tw;
+	const int mh = h * th;
+	pic = malloc(2 + 2 + mw * mh);
+	pic->w = mw;
+	pic->h = mh;
+#define FREAD(dest, size)\
+	if (fread(dest, size, 1, f) != 1) {\
+		fclose(f);\
+		goto bail;\
+	}
+	for (int x = 0; x < w; x++)
+	{
+		for (int y = 0; y < h; y++)
+		{
+			char flags;
+			FREAD(&flags, 1);
+			char sprite;
+			FREAD(&sprite, 1);
+			// Copy pixels over, one row of pixels at a time
+			const int px = x * tw;
+			const int py = y * th;
+			for (int row = 0; row < th; row++)
+			{
+				memcpy(
+					pic->data + px + (py + row) * mw,
+					pics[sprite]->data + row * tw,
+					tw);
+			}
+		}
+	}
+
+	PicPalettedToBMP(pic, palette, filename, 0, 1);
+
+bail:
+#undef FREAD
+	if (f != NULL)
+	{
+		fclose(f);
+	}
+	free(pic);
 }
 
 static void PicPalettedToBMP(
